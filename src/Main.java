886c1c529d7b40585e5d1d068d731e7beb0013c1
GBTC408007UR <GBTC408007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-06-20 10:02:48 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Cost\t\tQuantity\t\tTotal");
+        System.out.println("$1,000.00\t4\t\t\t\t$4,000.00");
+        System.out.println("$\t50.00\t8\t\t\t\t$400.00");
+        System.out.println("TOTAL:\t\t12\t\t\t\t$4,400.00");
+    }
+}
